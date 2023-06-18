1524c3d5c34280f729c31af1ffca5e9e1ab91341
VelkovIv <ivaylo.velkov@gmail.com>
2023-06-18 22:05:39 +0300

    App is ready

--- src/dkeeper/main.mo
+++ src/dkeeper/main.mo
@@ -1,5 +1,39 @@
-actor {
-  public func greet(name : Text) : async Text {
-    return "Hello, " # name # "!";
+import List "mo:base/List";
+import Text "mo:base/Text";
+import Debug "mo:base/Debug";
+
+
+actor DKeeper{
+
+  public type Note = {
+    title: Text;
+    content: Text;
   };
+
+  stable var notes: List.List<Note> = List.nil<Note>();
+
+  public func createNote(titleText: Text, contentText: Text) {
+
+    let newNote: Note = {
+      title = titleText;
+      content = contentText;
+    };
+
+      notes:= List.push(newNote, notes);
+      // Debug.print(debug_show(notes));
+  };
+
+  public query func readNotes(): async [Note] {
+      return List.toArray(notes);
+  };
+
+  public func removeNote(id: Nat) {
+    
+    let leftPart = List.take(notes, id);
+    let rightPart = List.drop(notes, id + 1);
+    notes:= List.append(leftPart, rightPart);
+    Debug.print(debug_show(notes));
+    
+  }
+
 };
--- src/dkeeper_assets/src/components/App.jsx
+++ src/dkeeper_assets/src/components/App.jsx
@@ -1,19 +1,34 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
+import { dkeeper } from "../../../declarations/dkeeper"
 
 function App() {
   const [notes, setNotes] = useState([]);
 
   function addNote(newNote) {
-    setNotes((prevNotes) => {
-      return [...prevNotes, newNote];
-    });
+    if (newNote.title !== "") {
+      setNotes((prevNotes) => {
+        dkeeper.createNote(newNote.title, newNote.content);
+        return [newNote, ...prevNotes];
+      });
+    };
+  }
+
+  useEffect(() =>{
+    console.log("useEfect trigered")
+    fetchData();
+  }, []);
+
+  async function fetchData() {
+    const notesArray = await dkeeper.readNotes();
+    setNotes(notesArray); 
   }
 
   function deleteNote(id) {
+    dkeeper.removeNote(id);
     setNotes((prevNotes) => {
       return prevNotes.filter((noteItem, index) => {
         return index !== id;
